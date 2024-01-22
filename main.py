da67850019804135ef7dc181bf73fc4ea9b87200
Siddharth Sharma <eshasidd@amazon.com>
2024-01-21 20:04:57 -0800

    refactored

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import argparse
-from transformers import AutoTokenizer
-from transformers import BartForConditionalGeneration, T5ForConditionalGeneration
-from transformers import BartConfig, T5Config
-from datasets import load_dataset
-from distill import DistillationTrainingArguments, DistillationTrainer
-from transformers import DataCollatorForSeq2Seq
+from seq2seq_distill_trainer import Seq2SeqDistillTrainer
 from utils import load_teacher_model, load_student_model, load_tokenizer, load_distill_dataset
 
 if __name__ == '__main__':
@@ -36,102 +31,6 @@ if __name__ == '__main__':
     parser.add_argument('optimizer', type=str, default='adamw_torch', help='optimizer for training')
     # Parse the arguments
     args = parser.parse_args()
-
-    # load tokenizer
-    tokenizer = load_tokenizer(args.model_type, args.custom_tokenizer_local_path, args.teacher)
-
-    vocab_size = tokenizer.vocab_size
-
-    # load student model
-    student_model = load_student_model(args.model_type, args.student_local_path, args.num_encoder_layers, args.num_decoder_layers, args.hidden_dim, vocab_size)
-
-    # load teacher model
-    teacher_model = load_teacher_model(args.model_type, args.teacher_local_path, args.teacher)
-
-    # load dataset
-    dataset = load_distill_dataset(args.dataset, args.dataset_local_path, args.dataset_data_type)
-
-    # tokenizer dataset function
-    # using closure for tokenizer since we would be using this function in generator and not in map function
-    def preprocess_function(sample,
-                        max_length: int,
-                        input_col_name: str,
-                        target_col_name: str,
-                        padding: str="max_length"):
-        """given input text dataset and tokenizer, tokenizer the text data for model training
-            params:
-               sample: text dataset element tp be tokenized
-               tokenizer: AutoTokenizer - huggingface tokenizer object
-               max_length: int - max sequence length
-               padding - padding strategy, pad to max length 
-               input_col_name: str - column with input data to model
-               output_col_name: str - column with correct spelling label
-           return - tokenized Dataset
-        """
-
-        # tokenize inputs
-        model_inputs = tokenizer(sample[input_col_name], max_length=max_length, padding=padding, truncation=True)
-
-        # tokenize labels/targets with the `text_target` keyword argument
-        labels = tokenizer(text_target=sample[target_col_name], max_length=max_length, padding=padding, truncation=True)
-
-        model_inputs["labels"] = labels["input_ids"]
-        return model_inputs
-    
-    # preprocess dataset
-    # instead of using map function to tokenize dataset function we would use generators
-    # https://huggingface.co/docs/datasets/processing.html#generators
-    # this is important for processing large datasets, else we would get out of disk space errors 
-    # since text to tokenization is space intensive (a char is 1 byte and a token is 4 bytes)
-    # train_dataset = dataset['train'].map(
-    #     preprocess_function,
-    #     batched=True,
-    #     remove_columns=[args.dataset_input_column, args.dataset_target_column],
-    # )
-
-    dataset.set_transform(preprocess_function)
-
-    # set training arguments
-    distill_training_args = DistillationTrainingArguments(
-        output_dir=args.output_dir, # output directory
-        num_train_epochs=args.epochs, # total number of training epochs
-        per_device_train_batch_size=args.batch_size, # batch size per device during training
-        per_device_eval_batch_size=args.batch_size, # batch size for evaluation
-        warmup_steps=500, # number of warmup steps for learning rate scheduler
-        weight_decay=0.01, # strength of weight decay
-        logging_dir='./logs', # directory for storing logs
-        logging_steps=10,
-        learning_rate=args.learning_rate,
-        eval_steps = args.logging_steps * 3,
-        evaluation_strategy="steps",
-        save_strategy = "no",
-        optim = args.optimizer,
-        fp16=args.fp16,
-        # distilation parameters
-        alpha=0.5,
-        temperature=4.0,
-        remove_unused_columns = False
-    )
-
-    # Data collator for Seq2Seq tasks: shifts the decoder input to the right by one position
-    seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=student_model, return_tensors = "pt")
-
-    # instantiate trainer
-    trainer = DistillationTrainer(
-        model=student_model, # the instantiated 🤗 Transformers model to be trained
-        args=distill_training_args, # training arguments, defined above
-        train_dataset=dataset["train"], # training dataset
-        eval_dataset=dataset["validation"], # evaluation dataset
-        data_collator=seq2seq_data_collator,
-        teacher_model=teacher_model
-    )
-
-    # start training
-    print('starting training')
-    trainer.train()
-
-    # save model
-    print(f"Saving model on disk - {args.output_dir}")
-    trainer.save_model(args.output_dir)
-
+    seq2seq_trainer = Seq2SeqDistillTrainer(args)
+    seq2seq_trainer.train()
 
---
+++ seq2seq_distill_trainer.py
@@ -0,0 +1,112 @@
+from transformers import AutoTokenizer
+from transformers import BartForConditionalGeneration, T5ForConditionalGeneration
+from transformers import BartConfig, T5Config
+from datasets import load_dataset
+from distill import DistillationTrainingArguments, DistillationTrainer
+from transformers import DataCollatorForSeq2Seq
+from utils import load_teacher_model, load_student_model, load_tokenizer, load_distill_dataset
+
+
+class Seq2SeqDistillTrainer:
+    def __init__(self, args):
+        self.output_dir = args.output_dir
+        self.num_train_epochs = args.epochs
+        self.per_device_train_batch_size = args.batch_size
+        self.per_device_eval_batch_size = args.batch_size
+        self.evaluation_strategy = "steps"
+        self.save_strategy = "no",
+        self.optim = args.optimizer,
+        self.fp16=args.fp16,
+        # distilation parameters
+        self.alpha=0.5,
+        self.temperature=4.0,
+        self.remove_unused_columns = False
+
+        # load tokenizer
+        self.tokenizer = load_tokenizer(args.model_type, args.custom_tokenizer_local_path, args.teacher)
+        self.vocab_size = self.tokenizer.vocab_size
+        # load student model
+        self.student_model = load_student_model(args.model_type, args.student_local_path, args.num_encoder_layers, args.num_decoder_layers, args.hidden_dim, self.vocab_size)
+        # load teacher model
+        self.teacher_model = load_teacher_model(args.model_type, args.teacher_local_path, args.teacher)
+        # load dataset
+        self.dataset = load_distill_dataset(args.dataset, args.dataset_local_path, args.dataset_data_type)
+
+        
+        # adding generator for dataset tokenization, it will be performed on the fly at the time of training
+        self.dataset.set_transform(self.preprocess_function)
+
+
+    # tokenizer dataset function
+    # using closure for tokenizer since we would be using this function in generator and not in map function
+    def preprocess_function(self, 
+                            sample,
+                        max_length: int,
+                        input_col_name: str,
+                        target_col_name: str,
+                        padding: str="max_length"):
+        """given input text dataset and tokenizer, tokenizer the text data for model training
+            params:
+            sample: text dataset element tp be tokenized
+            tokenizer: AutoTokenizer - huggingface tokenizer object
+            max_length: int - max sequence length
+            padding - padding strategy, pad to max length 
+            input_col_name: str - column with input data to model
+            output_col_name: str - column with correct spelling label
+        return - tokenized Dataset
+        """
+
+        # tokenize inputs
+        model_inputs = self.tokenizer(sample[input_col_name], max_length=max_length, padding=padding, truncation=True)
+
+        # tokenize labels/targets with the `text_target` keyword argument
+        labels = self.tokenizer(text_target=sample[target_col_name], max_length=max_length, padding=padding, truncation=True)
+
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+        
+
+    def train(self):
+        print('starting training')
+        # set training arguments
+        distill_training_args = DistillationTrainingArguments(
+            output_dir=self.output_dir, # output directory
+            num_train_epochs=self.epochs, # total number of training epochs
+            per_device_train_batch_size=self.batch_size, # batch size per device during training
+            per_device_eval_batch_size=self.batch_size, # batch size for evaluation
+            warmup_steps=500, # number of warmup steps for learning rate scheduler
+            weight_decay=0.01, # strength of weight decay
+            logging_dir='./logs', # directory for storing logs
+            logging_steps=100,
+            learning_rate=5e-5,
+            eval_steps = 200,
+            evaluation_strategy="steps",
+            save_strategy = "no",
+            optim = self.optimizer,
+            fp16=self.fp16,
+            # distilation parameters
+            alpha=0.5,
+            temperature=4.0,
+            remove_unused_columns = False
+        )
+        
+        # Data collator for Seq2Seq tasks: shifts the decoder input to the right by one position
+        seq2seq_data_collator = DataCollatorForSeq2Seq(self.tokenizer, model=self.student_model, return_tensors = "pt")
+
+        # instantiate trainer
+        trainer = DistillationTrainer(
+            model=self.student_model, # the instantiated 🤗 Transformers model to be trained
+            args=distill_training_args, # training arguments, defined above
+            train_dataset=self.dataset["train"], # training dataset
+            eval_dataset=self.dataset["validation"], # evaluation dataset
+            data_collator=seq2seq_data_collator,
+            teacher_model=self.teacher_model
+        )
+        # start training
+        self.trainer.train()
+        # save model
+        self.save_model() 
+
+    def save_model(self):
+        print(f"Saving model on disk - {self.output_dir}")
+        self.trainer.save_model(self.output_dir)

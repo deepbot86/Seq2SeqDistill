b877e28622c92dd2678f5eb6217d47e4a1ff6da1
Siddharth Sharma <eshasidd@amazon.com>
2024-01-21 20:58:17 -0800

    moved all load functions to util

--- utils.py
+++ utils.py
@@ -1,24 +1,90 @@
-def preprocess_function(sample,
-                        max_length: int,
-                        input_col_name: str,
-                        target_col_name: str,
-                        padding: str="max_length"):
-        """given input text dataset and tokenizer, tokenizer the text data for model training
-            params:
-               sample: text dataset element tp be tokenized
-               tokenizer: AutoTokenizer - huggingface tokenizer object
-               max_length: int - max sequence length
-               padding - padding strategy, pad to max length 
-               input_col_name: str - column with input data to model
-               output_col_name: str - column with correct spelling label
-           return - tokenized Dataset
-        """
+from transformers import AutoTokenizer
+from transformers import BartForConditionalGeneration, T5ForConditionalGeneration
+from transformers import BartConfig, T5Config
+from datasets import load_dataset
+from distill import DistillationTrainingArguments, DistillationTrainer
+from transformers import DataCollatorForSeq2Seq
 
-        # tokenize inputs
-        model_inputs = tokenizer(sample[input_col_name], max_length=max_length, padding=padding, truncation=True)
+def load_teacher_model(model_type: str, local_path: str, model_name: str) -> BartForConditionalGeneration or T5ForConditionalGeneration:
+    if model_type == 'bart':
+        #check if local path is provided
+        if local_path:
+            model = BartForConditionalGeneration.from_pretrained(local_path)
+        else:
+            model = BartForConditionalGeneration.from_pretrained(model_name)
+    elif model_type == 't5':
+        if local_path:
+            model = T5ForConditionalGeneration.from_pretrained(local_path)
+        else:
+            model = T5ForConditionalGeneration.from_pretrained(model_name)
+    else:
+        raise ValueError('model_type must be bart or t5')
+    return model
 
-        # tokenize labels/targets with the `text_target` keyword argument
-        labels = tokenizer(text_target=sample[output_col_name], max_length=args.max_length, padding=padding, truncation=True)
+def load_student_model(model_type: str, local_path: str, num_encoder_layers: str, num_decoder_layers:str, hidden_dim:int, vocab_size:str) -> BartForConditionalGeneration or T5ForConditionalGeneration:
+    if model_type == 'bart':
+        #check if local path is provided
+        if local_path:
+            student_model = BartForConditionalGeneration.from_pretrained(local_path)
+        else:
+            # load bart config
+            # Define the configuration
+            bart_config = BartConfig(
+                d_model=hidden_dim,  # dimensions of the model
+                encoder_layers=num_encoder_layers,  # number of encoder layers
+                decoder_layers=num_decoder_layers,  # number of decoder layers
+                vocab_size=vocab_size,  # vocabulary size
+            )
+            # Create the bart model from the configuration
+            student_model = BartForConditionalGeneration(bart_config)
 
-        model_inputs["labels"] = labels["input_ids"]
-        return model_inputs
+    elif model_type == 't5':
+        if local_path:
+            student_model = T5ForConditionalGeneration.from_pretrained(local_path)
+        else:
+            # load t5 config
+            # Define the configuration
+            t5_config = T5Config(
+                d_model=hidden_dim,  # dimensions of the model
+                encoder_layers=num_encoder_layers,  # number of encoder layers
+                decoder_layers=num_decoder_layers,  # number of decoder layers
+                vocab_size=vocab_size,  # vocabulary size
+            )
+            # Create the t5 model from the configuration
+            student_model = T5ForConditionalGeneration(t5_config)
+    else:
+        raise ValueError('model_type must be bart or t5')
+    return student_model
+
+def load_tokenizer(model_type: str, local_path: str, model_name: str) -> AutoTokenizer:
+    if model_type == 'bart':
+        #check if local path is provided
+        if local_path:
+            tokenizer = AutoTokenizer.from_pretrained(local_path)
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+    elif model_type == 't5':
+        if local_path:
+            tokenizer = AutoTokenizer.from_pretrained(local_path)
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+    else:
+        raise ValueError('model_type must be bart or t5')
+    return tokenizer
+
+def load_distill_dataset(dataset_name: str, local_path: str, dataset_data_type: str):
+        if local_path:
+            if dataset_data_type:
+                dataset = load_dataset(dataset_data_type, data_files={'train': local_path})
+            else:
+                raise ValueError('dataset_data_type must be provided while using local dataset path')
+        elif dataset_name:
+            dataset = load_dataset(dataset_name)
+        else:
+            raise ValueError('dataset_name or dataset_local_path must be provided') 
+        
+        # split dataset into train and validation
+        train_dataset = dataset['train'].train_test_split(test_size=0.1, shuffle=True)
+        train_dataset = train_dataset['train']
+        val_dataset = train_dataset['test']
+        return train_dataset, val_dataset
